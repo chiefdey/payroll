7bd0e874ee587c551306928c27c4d071a0cfa1a9
chiefdey <deynohmacharia@gmail.com>
2024-04-23 15:59:44 +0300

    two

--- payroll.js
+++ payroll.js
@@ -99,7 +99,7 @@ function calc_payee(tax) {
     let payee = 0
     let relief = 2400
     if (tax >= 0 && tax <= 24000) {
-        payee = (24000 * 0.01) - relief
+        payee = 0
     } else if (tax > 24000 &&
         tax <= 32333) {
         payee = (24000 * 0.01) + ((tax - 24000) * 0.25) - relief
